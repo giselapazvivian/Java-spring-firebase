598a1175d7800007c901f3788036153b4bc5601b
Gisela Donoso <gisela.donoso@23people.io>
2022-06-01 12:10:34 -0400

    Add restTemplate

--- src/main/java/com/example/ATS/Config.java
+++ src/main/java/com/example/ATS/Config.java
@@ -1,13 +1,13 @@
 package com.example.ATS;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@EnableJpaRepositories
 public class Config {
-    // @Bean
-    // public RestTemplate restTemplate() {
-    // return new RestTemplate();
-    // }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- src/main/java/com/example/ATS/WebSecurityConfig.java
+++ src/main/java/com/example/ATS/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/resttemplate/**").permitAll()
                 // .antMatchers("/applicants/**").permitAll()
                 .anyRequest().authenticated();
 
